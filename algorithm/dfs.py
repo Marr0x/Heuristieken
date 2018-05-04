1f41e435e3981962c38bd7b78fbb598542b8af20
mercylyn <m.d.wiemer@gmail.com>
2018-05-04 11:34:19 +0200

    dfs style

--- algorithm/dfs.py
+++ algorithm/dfs.py
@@ -1,48 +1,48 @@
 def rev(genes, x, y):
-  """ reverses a list of genes
+    """ reverses a list of genes
     genes = the list of integers
     x  = index of list where reversion should start
     y = index of list where reversion should end"""
-  
-  new_genes = genes[:]
 
-  if x is 0:
-    new_genes[:y+1] = new_genes[y::-1]
-    return new_genes
-  elif y < len(genes):
-    new_genes[x:y+1] = new_genes[y:x-1:-1]
-    return new_genes
-  else:
-    print("error1")
+    new_genes = genes[:]
+
+    if x is 0:
+        new_genes[:y+1] = new_genes[y::-1]
+        return new_genes
+    elif y < len(genes):
+        new_genes[x:y+1] = new_genes[y:x-1:-1]
+        return new_genes
+    else:
+        print("error1")
+
 
 def create_rev(genes):
 
-  stack = []
-  n = len(genes)
+    stack = []
+    n = len(genes)
 
-  # iterates over genes, decreasing every time
-  for i in range(n - 1 , 0, -1):
+    # iterates over genes, decreasing every time
+    for i in range(n - 1, 0, -1):
 
-    # x is the index of where the reversion starts
-    # y is the index of where the reversion ends
-    for j in range(0, i):
+        # x is the index of where the reversion starts
+        # y is the index of where the reversion ends
+        for j in range(0, i):
 
-      x = n - 1 - i
-      y = j + (n - i)
+            x = n - 1 - i
+            y = j + (n - i)
 
-      reversed_list = rev(genes, x, y)
+    reversed_list = rev(genes, x, y)
 
-      stack.append(reversed_list)
+    stack.append(reversed_list)
 
-  return stack
+    return stack
 
 
 def main():
 
-  genes = [5,4,3,2,1]
+    genes = [5, 4, 3, 2, 1]
 
-  print(create_rev(genes))
-  
+    print(create_rev(genes))
+
 if __name__ == "__main__":
-  main()
-
+    main()
