34d68c9970d6a3c99dea4092d6a398a503d0a3e1
alcyoneum <shanshan-huang@hotmail.com>
2018-05-09 10:08:40 +0200

    add fruitfly class

---
+++ algorithm/classes/Fruitfly.py
@@ -0,0 +1,17 @@
+from .helpers import load
+
+class Fruitfly(object):
+
+    def __init__(self, genome_file):
+        """ initialize with an array of genes from chosen fruitfly genome. """
+
+        self.genes = load.load_genome(genome_file)
+
+    def print_genes(self):
+        print(self.genes)
+
+    def __len__(self):
+        return len(self.genes)
+
+    def __str__(self):
+    	return str(self.genes)
