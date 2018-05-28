1bac3e05de92122fe1faa568d508e9a0a657c552
mercylyn <m.d.wiemer@gmail.com>
2018-05-28 14:38:05 +0200

    beam search

--- code/beamsearch.py
+++ code/beamsearch.py
@@ -15,38 +15,40 @@ def beamsearch(root_genome):
 
     while not solved:
 
-        # get current node with least points
-        genome = queue.pop(0)
+        if queue:
+            # get current node with least points
+            genome = queue.pop(0)
 
-        # show path to solution, when solution found
-        if genome == solution:
-            solved = True
-            print("Solution found in generation: ",
-                  genome.get_generation())
+            # show path to solution, when solution found
+            if genome == solution:
+                solved = True
+                print("Solution found in generation: ",
+                      genome.get_generation())
 
-            path = genome.path_solution()
-            print("Path to solution: ")
+                path = genome.path_solution()
+                print("Path to solution: ")
 
-            for inversion in range(len(path)):
-                print("Inversion: {:2d}".format(inversion), path[inversion])
+                for inversion in range(len(path)):
+                    print("Inversion: {:2d}".format(inversion), path[inversion])
+
+            else:
+                children = genome.create_children(Fruitfly.breakpoint_compare)
+
+                for child in children:
+                    heapq.heappush(priority_queue, child)
+                    current_layer = child.get_generation()
 
         else:
-
-            children = genome.create_children(Fruitfly.breakpoint_compare)
-
-            for child in children:
-                heapq.heappush(priority_queue, child)
-                print(child.get_generation())
-
-            for child in range(2):
+            for child in range(50):
                 best = heapq.heappop(priority_queue)
                 queue.append(best)
 
+            priority_queue[:] = []
 
 def main():
-    root_genome = [1,2,3,4,5]
+    # root_genome = [1,2,3,4,5]
+    root_genome = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
     np.random.shuffle(root_genome)
-    # root_genome = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
     fly = Fruitfly(root_genome)
     beamsearch(fly)
 
